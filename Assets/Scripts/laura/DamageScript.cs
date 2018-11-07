20ee773117777f9b546803c2d5a989f8a0dc3cbc
Laura Uhliarova <lau.uhl@gmail.com>
2018-11-07 16:31:07 +0100

    blaaa

---
+++ Assets/Scripts/laura/DamageScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageScript : MonoBehaviour {
+
+    float damage = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Bum");
+        other.gameObject.GetComponent<HealthScript>().TakeDamage(damage);
+    }
+
+}
+
---
+++ Assets/Scripts/laura/HealthScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour {
+
+    public Image healthBar;
+    public float max_health = 100F;
+    public float cur_health = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+
+        cur_health = max_health;
+        SetHealthBar();
+	}
+
+    public void TakeDamage(float amount) {
+
+        cur_health -= amount;
+        SetHealthBar();
+    }
+	// Update is called once per frame
+	void SetHealthBar() {
+        float my_health = cur_health / max_health;
+        healthBar.transform.localScale = new Vector3(Mathf.Clamp(my_health, 0F, 0f), healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+	}
+}
